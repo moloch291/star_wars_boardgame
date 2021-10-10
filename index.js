c58136be51de77bfeba6da24d1944469b20f5994
moloch291 <danielkomjati1@gmail.com>
2021-10-10 16:51:39 +0200

    fix stylesheet and other static references

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const favicon = require('express-favicon');
+const nunjucks = require("nunjucks");
 // const unirest = require("unirest");
 const path = require("path");
-const port = 3000;
-const htmlFilePrefix = "/../../templates/";
+const port = 8000;
+const htmlFilePrefix = "/templates/";
 
 const app = express();
-app.use(favicon(__dirname + "../../../static/img/favicon_deathStar.png"));
+nunjucks.configure('./../../templates', {express: app});
+app.set('views', __dirname + htmlFilePrefix);
+app.use(favicon(__dirname + "/static/img/favicon_deathStar.png"));
+app.use("/static", express.static(path.join(__dirname + "/static")));
+
 
 app.get("/", (req, res) => {
     res.sendFile('index.html', {
@@ -16,4 +21,5 @@ app.get("/", (req, res) => {
 
 app.listen(port, () => {
     console.log(`App is listening at localhost on port ${port}!`);
+    console.log(__dirname);
 });
