ac1f89b84a26f15323c979080697a9fdaea50da5
moloch291 <danielkomjati1@gmail.com>
2021-10-14 16:38:40 +0200

    re-add react

--- backend/server.js
+++ backend/server.js
@@ -3,12 +3,12 @@ const favicon = require('express-favicon');
 // const unirest = require("unirest");
 const path = require("path");
 const port = 8000;
-const htmlFilePrefix = "/../frontend/templates/";
+const htmlFilePrefix = "/../frontend2/public/";
 
 const app = express();
 app.set('views', __dirname + htmlFilePrefix);
-app.use(favicon(__dirname + "/../frontend/static/img/favicon_deathStar.png"));
-app.use("/static", express.static(path.join(__dirname + "/../frontend/static")));
+app.use(favicon(__dirname + "/../frontend2/public/img/favicon_deathStar.png"));
+app.use("/static", express.static(path.join(__dirname + "/../frontend2/static")));
 
 
 app.get(
