e86c94a82bd8c15488677557ebb83fef00ce58e0
moloch291 <danielkomjati1@gmail.com>
2021-11-18 14:08:27 +0100

    start form-design implementation

--- frontend/src/components/LoginAndRegister.js
+++ frontend/src/components/LoginAndRegister.js
@@ -1,4 +1,5 @@
 function LoginAndRegister() {
+    // FUNCTION(S) CALLED BY COMPONENTS:
     // Fade in or out elements in component:
     async function fadeElement(elementClassName, mode, modeToRemove) {
         const elementToFade = document.querySelector(elementClassName);
@@ -7,63 +8,75 @@ function LoginAndRegister() {
         await elementToFade.classList.add(mode);
         setTimeout(() => elementToFade.removeAttribute("hidden"), 1000);
     }
-    // Login form:
+    // Login form component:
     function LoginForm() {
         return (
             <div className="loginForm" hidden>
-                <label htmlFor="user-email">Email:</label>
-                <input type="email" id="user-email" name="user-email" placeholder="Email"/>
-                <label htmlFor="user-password">Password:</label>
-                <input type="password" id="user-password" name="user-password" placeholder="Password"/>
-                <button>Log in!</button>
-                <p>New to the game?</p>
-                <button onClick={
-                    async () => {
-                        await fadeElement(
-                            ".loginForm", "fadeOut", "fadeIn"
-                        );
-                        await fadeElement(
-                            ".registrationForm", "fadeIn", "fadeOut"
-                        );
-                        setTimeout(
-                            () => document.querySelector(".loginForm")
-                                .setAttribute("hidden", "true"),
-                            1000
-                        );
-                    }
-                }>Register!</button>
+                <form>
+                    <h3>Login</h3>
+                    <label htmlFor="user-email">Email:</label>
+                    <input type="email" id="user-email" name="user-email" placeholder="Email"/>
+                    <label htmlFor="user-password">Password:</label>
+                    <input type="password" id="user-password" name="user-password" placeholder="Password"/>
+                    <button className="formButton" onClick={
+                        (e) => e.preventDefault()
+                    }>Log in!</button>
+                    <p>New to the game?</p>
+                    <button className="formButton" onClick={
+                        async (e) => {
+                            e.preventDefault();
+                            await fadeElement(
+                                ".loginForm", "fadeOut", "fadeIn"
+                            );
+                            await fadeElement(
+                                ".registrationForm", "fadeIn", "fadeOut"
+                            );
+                            setTimeout(
+                                () => document.querySelector(
+                                    ".loginForm"
+                                ).setAttribute("hidden", "true"),
+                                1000
+                            );
+                        }
+                    }>Register!</button>
+                </form>
             </div>
         );
     }
-    // Registration form:
+    // Registration form component:
     function RegistrationForm() {
         return (
             <div className="registrationForm" hidden>
-                <label htmlFor="username">Username:</label>
-                <input type="text" id="username" name="username"/>
-                <label htmlFor="user-email">Email:</label>
-                <input type="email" id="user-email" name="user-email"/>
-                <label htmlFor="user-password">Password:</label>
-                <input type="password" id="user-password" name="user-password"/>
-                <label htmlFor="user-password-again">Password again:</label>
-                <input type="password" id="user-password-again" name="user-password-again"/>
-                <button id="subscribe">Register!</button>
-                <p>Already have an account?</p>
-                <button onClick={
-                    async () => {
-                        await fadeElement(
-                            ".registrationForm", "fadeOut", "fadeIn"
-                        );
-                        await fadeElement(
-                            ".loginForm", "fadeIn", "fadeOut"
-                        );
-                        setTimeout(
-                            () => document.querySelector(".registrationForm")
-                                .setAttribute("hidden", "true"),
-                            1000
-                        );
-                    }
-                }>Log in!</button>
+                <form>
+                    <label htmlFor="username">Username:</label>
+                    <input type="text" id="username" name="username"/>
+                    <label htmlFor="user-email">Email:</label>
+                    <input type="email" id="user-email" name="user-email"/>
+                    <label htmlFor="user-password">Password:</label>
+                    <input type="password" id="user-password" name="user-password"/>
+                    <label htmlFor="user-password-again">Password again:</label>
+                    <input type="password" id="user-password-again" name="user-password-again"/>
+                    <button className="formButton" onClick={
+                        (e) => e.preventDefault()
+                    }>Register!</button>
+                    <p>Already have an account?</p>
+                    <button className="formButton" onClick={
+                        async (e) => {
+                            e.preventDefault();
+                            await fadeElement(
+                                ".registrationForm", "fadeOut", "fadeIn"
+                            );
+                            await fadeElement(
+                                ".loginForm", "fadeIn", "fadeOut"
+                            );
+                            setTimeout(
+                                () => document.querySelector(".registrationForm")
+                                    .setAttribute("hidden", "true"),
+                                1000
+                            );
+                        }
+                    }>Log in!</button>
+                </form>
             </div>
         );
     }
@@ -76,7 +89,6 @@ function LoginAndRegister() {
                 <button className="glow-on-hover" id="login"
                         onClick={
                             async () => {
-                                //await showLoginForm();
                                 const xWingFire = document.querySelector(".xWingFire");
                                 xWingFire.play();
                                 await fadeElement(
@@ -102,7 +114,6 @@ function LoginAndRegister() {
                 <button className="glow-on-hover" id="registration"
                         onClick={
                             async () => {
-                                //await showRegistrationForm();
                                 const tieFighterFire = document.querySelector(".TieFighterFire");
                                 tieFighterFire.play();
                                 await fadeElement(
