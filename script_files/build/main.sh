21d12470fd26bd2b2191e34091e0de8300227c05
moloch291 <danielkomjati1@gmail.com>
2022-01-22 14:32:54 +0100

    extract install method

--- script_files/build/main.sh
+++ script_files/build/main.sh
@@ -1,23 +1,10 @@
 #!/bin/bash
 
-handle_kubectl() {
-    kubectl version --client
-    if [ $? -ne 0 ]; then
-        echo "Installing kubectl..."
-        curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-        curl -LO "https://dl.k8s.io/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
-        VALIDATION=$(echo "$(<kubectl.sha256)  kubectl" | sha256sum --check)
-        if [ "$VALIDATION" -eq "kubectl: OK" ]; then
-            install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
-        else
-            echo "$VALIDATION"
-        fi
-    fi
-}
+. install_kubectl.sh
 
 main() {
     echo "Building dependecies..."
-    handle_kubectl
+    sh install_kubectl.sh
 }
 
 main
