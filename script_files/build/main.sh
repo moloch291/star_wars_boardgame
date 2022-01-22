890260c71109f4015990091b51979cd22365b8eb
moloch291 <danielkomjati1@gmail.com>
2022-01-22 14:20:14 +0100

    install kubectl binary in main build script

--- script_files/build/main.sh
+++ script_files/build/main.sh
@@ -4,12 +4,14 @@ handle_kubectl() {
     kubectl version --client
     if [ $? -ne 0 ]; then
         echo "Installing kubectl..."
-        apt-get update
-        apt-get install -y apt-transport-https ca-certificates curl
-        curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
-        echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | tee /etc/apt/sources.list.d/kubernetes.list
-        apt-get update
-        apt-get install -y kubectl
+        curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+        curl -LO "https://dl.k8s.io/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
+        VALIDATION=$(echo "$(<kubectl.sha256)  kubectl" | sha256sum --check)
+        if [ "$VALIDATION" -eq "kubectl: OK" ]; then
+            install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+        else
+            echo "$VALIDATION"
+        fi
     fi
 }
 
