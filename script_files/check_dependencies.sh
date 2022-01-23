ea4bded01fe579f81a04c8eca25529c99ac46276
moloch291 <danielkomjati1@gmail.com>
2022-01-23 19:37:25 +0100

    try to install minikube again

--- script_files/check_dependencies.sh
+++ script_files/check_dependencies.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
 handle_minikube() {
-    local VERSION=$(minikube version)
+    minikube version
     if [ $? -ne 0 ]; then
         apt-get install apt-transport-https
         apt install virtualbox virtualbox-ext-pack
         wget https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
         cp minikube-linux-amd64 /usr/local/bin/minikube
         chmod 755 /usr/local/bin/minikube
-        echo "Minikube installed!"
+        echo "minikube installed!"
         minikube version
     else
         echo "* 'minikube' already installed, skipping this step... *"
-        echo "$VERSION"
+        echo "$(minikube version)"
     fi
 
 }
 
 handle_kubectl() {
-    local VERSION=$(kubectl version --client)
+    kubectl version --client
     if [ $? -ne 0 ]; then
         apt-get update
         apt-get install -y apt-transport-https ca-certificates curl
@@ -26,10 +26,11 @@ handle_kubectl() {
         echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
         apt-get update
         apt-get install -y kubectl
+        echo "kubectl installed!"
         kubectl version --client
     else
         echo "* 'kubectl' already installed! skipping this step... *"
-        echo "$VERSION"
+        echo "$(kubectl version --client)"
     fi
 }
 
