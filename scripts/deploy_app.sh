af8b89e16312c47efc9f7c684d9f143494332fc1
moloch291 <danielkomjati1@gmail.com>
2022-01-25 16:50:07 +0100

    adjust deployment script to display results

--- scripts/deploy_app.sh
+++ scripts/deploy_app.sh
@@ -1,3 +1,11 @@
+describe_details() {
+    kubectl get pods
+    kubectl get deployment
+    kubectl get svc
+    echo "LoadBalancer service details:"
+    kubectl describe svc sw-boardgame-service
+}
+
 delete_previous_version() {
     kubectl delete deployment sw-boardgame-deployment
     kubectl delete svc sw-boardgame-service
@@ -11,6 +19,7 @@ redeploy() {
 main() {
     delete_previous_version
     redeploy
+    describe_details
 }
 
 main
