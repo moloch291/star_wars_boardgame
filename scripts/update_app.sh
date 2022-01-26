69710827b01110baf32834febe4fadfd414a8808
moloch291 <danielkomjati1@gmail.com>
2022-01-26 11:52:24 +0100

    add refactored script

---
+++ scripts/update_app.sh
@@ -0,0 +1,21 @@
+describe_details() {
+    kubectl get pods
+    kubectl get deployment
+    kubectl get svc
+    echo "LoadBalancer service details:"
+    kubectl describe svc sw-boardgame-service
+    echo "Helm details:"
+    helm ls
+}
+
+redeploy() {
+    helm upgrade sw-bg ./helm/
+}
+
+main() {
+    delete_previous_version
+    redeploy
+    describe_details
+}
+
+main
