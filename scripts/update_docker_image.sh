db5dae91d419bedc58575410ca508afa01545494
moloch291 <danielkomjati1@gmail.com>
2022-01-26 12:27:23 +0100

    upgrade debug

--- scripts/update_docker_image.sh
+++ scripts/update_docker_image.sh
@@ -1,11 +1,11 @@
 tag_and_push() {
-    docker tag sw-boardgame:0.1.0 872533758794.dkr.ecr.eu-central-1.amazonaws.com/sw-boardgame:0.1.0
-    docker push 872533758794.dkr.ecr.eu-central-1.amazonaws.com/sw-boardgame:0.1.0
+    docker tag sw-boardgame:latest 872533758794.dkr.ecr.eu-central-1.amazonaws.com/sw-boardgame:latest
+    docker push 872533758794.dkr.ecr.eu-central-1.amazonaws.com/sw-boardgame:latest
 }
 
 build_image() {
     cd ./frontend
-    docker build -t sw-boardgame:0.1.0 .
+    docker build -t sw-boardgame .
     cd ..
 }
 
